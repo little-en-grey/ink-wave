1155fbc0fea1c0c56589495c3754effffd239bb9
little-en-grey <little.en.grey@gmail.com>
2024-07-21 01:10:00 +0900

    サムネ追加

--- ink-wave/components/todays_result2.vue
+++ ink-wave/components/todays_result2.vue
@@ -78,8 +78,11 @@
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer />
-                    <v-btn color="primary" @click="generateImage"> スライド作成 </v-btn>
-                    <v-btn color="red darken-3" @click="generateImageThumbnail">サムネイル作成</v-btn>
+                    <v-btn color="red darken-3" @click="generateImage"> スライド作成 </v-btn>
+                    <v-btn color="light-blue" @click="generateImageNormalThumbnail('aqua')">サムネイル作成</v-btn>
+                    <v-btn color="blue-darken-4" @click="generateImageNormalThumbnail('blue')">サムネイル作成</v-btn>
+                    <v-btn variant="flat" color="light-blue" @click="generateImageThumbnail('aqua')">ATLANTICサムネイル作成</v-btn>
+                    <v-btn variant="flat" color="blue-darken-4" @click="generateImageThumbnail('blue')">PACIFICサムネイル作成</v-btn>
                 </v-card-actions>
             </v-card>
             <v-card v-show="imageDataURL" class="pt-1 pr-1 pl-1">
@@ -113,7 +116,10 @@ const imageDataURL = ref(null)
 const imageName = ref('generated_image.png')
 const baseImageUrl = ref('/ink-wave/todays_result/todays_result2.jpg')
 const defaultLogoPath = ref('/ink-wave/default_logo/')
-const thumbnailImageUrl = ref('/ink-wave/todays_result/thumb.jpg')
+const thumbnailImageBlueUrl = ref('/ink-wave/todays_result/thumb.jpg')
+const thumbnailImageAquaUrl = ref('/ink-wave/todays_result/thumb_end_A.jpg')
+const thumbnailImageAtlantiicUrl = ref('/ink-wave/todays_result/thumb_end_ATLANTIC.jpg')
+const thumbnailImagePacificUrl = ref('/ink-wave/todays_result/thumb_end_PACIFIC.jpg')
 const canvasWidth = ref(1920)
 const canvasHeight = ref(1080)
 
@@ -239,13 +245,18 @@ const generateImage = async (): Promise<Boolean> => {
         imageDataURL.value = canvas.value.toDataURL();
     }
 }
-const generateImageThumbnail = async (): Promise<Boolean> => {
+const generateImageNormalThumbnail = async (type: string): Promise<Boolean> => {
     canvas.value = document.querySelector('#imageCanvas');
     if (!canvas || !canvas.value.getContext) return false;
     context.value = canvas.value.getContext('2d')
 
     const baseImage = new Image();
-    baseImage.src = thumbnailImageUrl.value;
+    if (type == 'blue') {
+        baseImage.src = thumbnailImageBlueUrl.value;
+    } else {
+        baseImage.src = thumbnailImageAquaUrl.value;
+    }
+    
 
     baseImage.onload = () => {
         context.value.drawImage(baseImage, 0, 0, canvasWidth.value, canvasHeight.value);
@@ -366,10 +377,184 @@ const generateImageThumbnail = async (): Promise<Boolean> => {
         }
         
         context.value.font = `bold ${fontSize}px 'Noto Sans JP', sans-serif`;
-        context.value.fillStyle = '#332771';
-
+        if (type == 'blue') {
+            context.value.fillStyle = '#332771';
+        } else {
+            context.value.fillStyle = '#2282AA';
+        }
         const x = (295 + 1044 - context.value.measureText( seasonNo.value ).width) / 2 + 1044 / 2;
         context.value.fillText(seasonNo.value, x, 126);
+        console.log('seasonTextWidth:' + seasonTextWidth);
+        console.log('x:' + x);
+
+        // 実況解説アイコン
+        const iconMaxSize = 211;
+        const iconTop = 68;
+        const commentaryImage = new Image();
+        if (logoImageCommentary.value && logoImageCommentary.value[0].type && logoImageCommentary.value[0].type.match('image/')) {
+            commentaryImage.src = URL.createObjectURL(logoImageCommentary.value[0]);
+            commentaryImage.onload = () => {
+
+                context.value.drawImage(commentaryImage, 282 - 60, iconTop, iconMaxSize, iconMaxSize);
+                URL.revokeObjectURL(commentaryImage.src);
+                imageDataURL.value = canvas.value.toDataURL();
+            }
+        }
+
+        const explanationImage = new Image();
+        if (logoImageExplanation.value && logoImageExplanation.value[0].type && logoImageExplanation.value[0].type.match('image/')) {
+            explanationImage.src = URL.createObjectURL(logoImageExplanation.value[0]);
+            explanationImage.onload = () => {
+                context.value.drawImage(explanationImage, 1427 + 60, iconTop, iconMaxSize, iconMaxSize);
+                URL.revokeObjectURL(explanationImage.src);
+                imageDataURL.value = canvas.value.toDataURL();
+            }
+        }
+
+
+        imageDataURL.value = canvas.value.toDataURL();
+    }
+}
+
+const generateImageThumbnail = async (type: string): Promise<Boolean> => {
+    canvas.value = document.querySelector('#imageCanvas');
+    if (!canvas || !canvas.value.getContext) return false;
+    context.value = canvas.value.getContext('2d')
+
+    const baseImage = new Image();
+    if (type == 'blue') {
+        baseImage.src = thumbnailImagePacificUrl.value;
+    } else {
+        baseImage.src = thumbnailImageAtlantiicUrl.value;
+    }
+
+    baseImage.onload = () => {
+        context.value.drawImage(baseImage, 0, 0, canvasWidth.value, canvasHeight.value);
+
+        // Draw text
+        context.value.fillStyle = 'white';
+
+        // チームロゴ
+        const maxWidth = 164;
+        const maxHeight = 164;
+
+        const landingA = 139;
+        const landingB = 1607;
+        const top = 325;
+
+        for (let i = 0; i < count.value; i++) {
+            const logoA = new Image();
+            const logoB = new Image();
+
+            let issetImageA = Array.isArray(logoImageA.value[i]) && logoImageA.value[i][0] !== undefined
+            let issetImageB = Array.isArray(logoImageB.value[i]) && logoImageB.value[i][0] !== undefined
+
+            if ((issetImageA && logoImageA.value[i][0].type && logoImageA.value[i][0].type.match('image/')) || defaultLogoA.value[i]) {
+                if (issetImageA && logoImageA.value[i][0].type && logoImageA.value[i][0].type.match('image/')) {
+                    logoA.src = URL.createObjectURL(logoImageA.value[i][0]); // ファイルからURLを生成
+                } else {
+                    logoA.src = selectLogo(defaultLogoA.value[i]);
+                }
+
+                logoA.onload = () => {
+                    let logoWidth = maxWidth;
+                    let logoHeight = maxHeight;
+                    // 比率が1:1の場合
+                    if (logoA.width === logoA.height) {
+                        logoWidth = logoA.width <= maxWidth ? logoA.width : maxWidth;
+                        logoHeight = logoA.height <= maxHeight ? logoA.height : maxHeight;
+                    } else if (logoA.width > logoA.height) { // ヨコが長い
+                        logoWidth = logoA.width <= maxWidth ? logoA.width : maxWidth;
+                        const ratio = logoA.width / logoWidth;
+                        logoHeight = logoA.height / ratio;
+                    } else { // タテが長い
+                        logoHeight = logoA.height <= maxHeight ? logoA.height : maxHeight;
+                        const ratio = logoA.height / logoHeight;
+                        logoWidth = logoA.width / ratio;
+                    }
+
+                    const x = (maxWidth + landingA - logoWidth) / 2 + landingA / 2;
+                    const y = (maxHeight + (top + (i * 180)) - logoHeight) / 2 + (top + (i * 180)) / 2;
+                    context.value.drawImage(logoA, x, y, logoWidth, logoHeight);
+                    if (issetImageA && logoImageA.value[i][0].type && logoImageA.value[i][0].type.match('image/')) URL.revokeObjectURL(logoA.src); // 不要になったURLを解放
+                    imageDataURL.value = canvas.value.toDataURL();
+                }
+            }
+
+            if ((issetImageB && logoImageB.value[i][0].type && logoImageB.value[i][0].type.match('image/')) || defaultLogoB.value[i]) {
+                if (issetImageB && logoImageB.value[i][0].type && logoImageB.value[i][0].type.match('image/')) {
+                    logoB.src = URL.createObjectURL(logoImageB.value[i][0]); // ファイルからURLを生成
+                } else {
+                    logoB.src = selectLogo(defaultLogoB.value[i]);
+                }
+
+                logoB.onload = () => {
+                    let logoWidth = maxWidth;
+                    let logoHeight = maxHeight;
+                    // 比率が1:1の場合
+                    if (logoB.width === logoB.height) {
+                        logoWidth = logoB.width <= maxWidth ? logoB.width : maxWidth;
+                        logoHeight = logoB.height <= maxHeight ? logoB.height : maxHeight;
+                    } else if (logoB.width > logoB.height) { // ヨコが長い
+                        logoWidth = logoB.width <= maxWidth ? logoB.width : maxWidth;
+                        const ratio = logoB.width / logoWidth;
+                        logoHeight = logoB.height / ratio;
+                    } else { // タテが長い
+                        logoHeight = logoB.height <= maxHeight ? logoB.height : maxHeight;
+                        const ratio = logoB.height / logoHeight;
+                        logoWidth = logoB.width / ratio;
+                    }
+
+                    const x = (maxWidth + landingB - logoWidth) / 2 + landingB / 2;
+                    const y = (maxHeight + (top + (i * 180)) - logoHeight) / 2 + (top + (i * 180)) / 2;
+                    context.value.drawImage(logoB, x, y, logoWidth, logoHeight);
+                    if (issetImageB && logoImageB.value[i][0].type && logoImageB.value[i][0].type.match('image/')) URL.revokeObjectURL(logoB.src); // 不要になったURLを解放
+                    imageDataURL.value = canvas.value.toDataURL();
+                }
+            }
+
+            // TEAM A
+            context.value.textAlign = "right";
+            context.value.font = "bold 50px 'Noto Sans JP', sans-serif";
+            if (inputTeamNameA.value[i] !== undefined) context.value.fillText(inputTeamNameA.value[i], 855, 430 + (i * 180));
+
+            // TEAM B
+            context.value.textAlign = "left";
+            if (inputTeamNameB.value[i] !== undefined) context.value.fillText(inputTeamNameB.value[i], 1063, 430 + (i * 180));
+        }
+        // Day
+        const dayMaxWidth = 125;
+        let fontSize = 120;
+        const dayTextWidth = context.value.measureText( dayNo.value ).width;
+        // 横幅が最大幅を超える場合は、フォントサイズを調整
+        if (dayTextWidth > dayMaxWidth) {
+            // 適当なフォントサイズに調整するロジックを追加
+            fontSize = Math.floor(fontSize * (dayMaxWidth / dayTextWidth));
+        }
+        context.value.font = `bold ${fontSize}px 'Noto Sans JP', sans-serif`;
+        context.value.fillStyle = 'white';
+        context.value.fillText(dayNo.value, 1038, 220);
+
+        // Season
+        const seasonMaxWidth = 900;
+        fontSize = 40;
+        // const splitSeasonText = this.seasonNo.split('').join(' ');
+        const seasonTextWidth = context.value.measureText( seasonNo.value ).width;
+        // 横幅が最大幅を超える場合は、フォントサイズを調整
+        if (seasonTextWidth > seasonMaxWidth) {
+            // 適当なフォントサイズに調整するロジックを追加
+            fontSize = Math.floor(fontSize * (seasonMaxWidth / seasonTextWidth));
+        }
+        
+        context.value.font = `bold ${fontSize}px 'Noto Sans JP', sans-serif`;
+        if (type == 'blue') {
+            context.value.fillStyle = '#332771';
+        } else {
+            context.value.fillStyle = '#2282AA';
+        }
+
+        const x = (295 + 1044 - context.value.measureText( seasonNo.value ).width) / 2 + 1044 / 2;
+        context.value.fillText(seasonNo.value, x, 116);
         console.log('seasonTextWidth:' + seasonTextWidth);
         console.log('x:' + x);
 
